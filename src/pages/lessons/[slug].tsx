50bb3d6ac0b988328d0fdca52bcdde1cf52f2c7f
Rahat Chowdhury <rahatchowdhury@Rahats-MacBook-Pro.local>
2021-12-31 00:45:02 -0500

    chore: configure mdx

---
+++ src/pages/lessons/[slug].tsx
@@ -0,0 +1,94 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import { MDXRemote } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import Link from 'next/link';
+import path from 'path';
+import { lessonFilePaths, LESSONS_PATH } from '../../utils/mdxUtils';
+
+// Custom components/renderers to pass to MDX.
+// Since the MDX files aren't loaded by webpack, they have no knowledge of how
+// to handle import statements. Instead, you must include components in scope
+// here.
+const components = {
+  ConnectWallet: dynamic(() => import(`@/components/ConnectWallet`)),
+  Head,
+};
+
+export default function LessonPage({ source, frontMatter }) {
+  console.log({ source, frontMatter });
+  return (
+    <>
+      <header>
+        <nav>
+          <Link href="/">
+            <a>👈 Go back home</a>
+          </Link>
+        </nav>
+      </header>
+      <div className="post-header">
+        <h1>{frontMatter.title}</h1>
+        {frontMatter.description && (
+          <p className="description">{frontMatter.description}</p>
+        )}
+      </div>
+      <main>
+        <MDXRemote {...source} components={components} />
+      </main>
+
+      <style jsx>{`
+        .post-header h1 {
+          margin-bottom: 0;
+        }
+        .post-header {
+          margin-bottom: 2rem;
+        }
+        .description {
+          opacity: 0.6;
+        }
+      `}</style>
+    </>
+  );
+}
+
+// interface Props {
+//   params: string;
+// }
+
+export const getStaticProps = async ({ params }) => {
+  console.log({ params });
+  const postFilePath = path.join(LESSONS_PATH, `${params.slug}.mdx`);
+  const source = fs.readFileSync(postFilePath);
+
+  const { content, data } = matter(source);
+
+  const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    },
+    scope: data,
+  });
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = lessonFilePaths
+    // Remove file extensions for page paths
+    .map((path: string) => path.replace(/\.mdx?$/, ``))
+    // Map the path into the static paths object required by Next.js
+    .map((slug: string) => ({ params: { slug } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
