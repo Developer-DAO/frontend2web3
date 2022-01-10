f4a18f76a5a65d87ab49a2239253b327067146f2
Rahat Chowdhury <rahatchowdhury@rahats-mbp.myfiosgateway.com>
2022-01-10 17:37:45 -0500

    feat: in progress videos and lessons page

---
+++ src/pages/lessons/index.tsx
@@ -0,0 +1,5 @@
+const Lessons = () => {
+  return <h1>In progress</h1>;
+};
+
+export default Lessons;
---
+++ src/pages/videos/index.tsx
@@ -0,0 +1,5 @@
+const Videos = () => {
+  return <h1>In progress</h1>;
+};
+
+export default Videos;
