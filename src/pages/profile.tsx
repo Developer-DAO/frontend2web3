43212816caedacea5ae26cc3bebe5567bc7fe786
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2022-01-31 18:49:42 +0100

    feat: added modal for form

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,7 @@
 import { UserContext } from '@/contexts/UserContext';
 import Image from 'next/image';
 import * as React from 'react';
+import ReactModal from 'react-modal';
 import { useAccount } from 'wagmi';
 import styles from './pageStyles/profile.module.css';
 export interface IProfileProps {
@@ -8,6 +9,8 @@ export interface IProfileProps {
 }
 
 export default function Profile(props: IProfileProps) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   const { authenticated } = React.useContext(UserContext);
   const [{ data }] = useAccount({ fetchEns: true });
   const isAuthenticated = authenticated && data;
@@ -37,7 +40,37 @@ export default function Profile(props: IProfileProps) {
               : `No connected`}
           </div>
         </div>
+
+        <div
+          role="button"
+          onClick={() => setIsOpen(true)}
+          className={styles.add__info}
+        ></div>
       </div>
+
+      <ReactModal
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        contentLabel={`Select A Wallet Connect Option`}
+        portalClassName={styles.modal}
+        style={{
+          overlay: {
+            backgroundColor: `#0a0909ab`,
+          },
+          content: {
+            maxWidth: `865px`,
+            maxHeight: `532px`,
+            width: `81%`,
+            height: `100%`,
+            margin: `5% auto`,
+            backgroundColor: `#343436`,
+            border: `1px solid black`,
+            borderRadius: `15px`,
+          },
+        }}
+      >
+        <div>Modal</div>
+      </ReactModal>
     </div>
   );
 }
