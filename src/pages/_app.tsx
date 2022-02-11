194dbd0d59b1b88265543791388e2790185a2e55
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2022-02-11 21:36:16 +0100

    fix: removed unused imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import { WalletLinkConnector } from 'wagmi/connectors/walletLink';
 
 import UserProvider from '@/contexts/UserContext';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { DefaultSeo, NextSeo } from 'next-seo';
+import { DefaultSeo } from 'next-seo';
 import SEO from '../config/next-seo.config';
 // import your default seo configuration
 const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import styles from './pageStyles/index.module.css';
 import Image from 'next/image';
 import Features from '@/components/LandingPage/Features';
